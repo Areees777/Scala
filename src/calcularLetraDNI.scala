5b26d6fdfad65ff6b8747c1fee56d5557847732a
Ares <53051330+Areees777@users.noreply.github.com>
2020-02-13 13:15:38 +0100

    Update calcularLetraDNI.scala

--- src/calcularLetraDNI.scala
+++ src/calcularLetraDNI.scala
@@ -1,4 +1,8 @@
-def quitarAcentos(registros: DataFrame): DataFrame = {
+val abecedarioUpper = Seq("A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z").toDF("abecedarioUpper").collect().map(_(0)).toList
+val abecedarioLower = Seq("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z").toDF("abecedarioLower").collect().map(_(0)).toList
+val letraNIF = "TRWAGMYFPDXBNJZSQVHLCKE"
+
+def calcularLetraDNI(registros: DataFrame): DataFrame = {
     val docsDoradosSinLetra = registros.filter(length($"TXTO_DOCUMENTO") === "8" && (!substring($"TXTO_DOCUMENTO", 8, 1).isin(abecedarioUpper: _*) || !substring($"TXTO_DOCUMENTO", 8, 1).isin(abecedarioLower: _*)))            
     val calculoLetraDorada = docsDoradosSinLetra.withColumn("LetraCorrecta", when(substring($"TXTO_DOCUMENTO", 1, 1).geq(0) && substring($"TXTO_DOCUMENTO", 1, 1).leq(9), $"TXTO_DOCUMENTO"))            
     val letraDoradaCorrecta = calculoLetraDorada.withColumn("LetraCorrecta", when($"LetraCorrecta".isNotNull, limpiezaLetra)).filter($"LetraCorrecta".isNotNull && $"LetraCorrecta" =!= "INVALID_ID")            
